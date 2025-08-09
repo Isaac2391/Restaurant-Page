67f68a16bc6ddc3ecc43909aa7450f4827a9f69d
Isaac Wilson <isaacrw248@gmail.com>
2025-08-09 17:04:48 +0100

    Initial files

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { renderPage } from "./mainModule.js"
+
+renderPage()
---
+++ src/mainModule.js
@@ -0,0 +1,13 @@
+const renderPage = function() {
+
+let body = document.querySelector("body")
+let content = document.querySelector("#content")
+
+content.style.width = "100vw"
+content.style.height = "100vh"
+
+body.style.backgroundColor = "maroon"
+
+}
+
+export {renderPage} 
