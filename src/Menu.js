663fb6b10c7f37e17d70e999f02d1c6fd1df7cd7
Isaac Wilson <isaacrw248@gmail.com>
2025-08-16 10:30:54 +0100

    Added Menu page and tab switching

--- src/Menu.js
+++ src/Menu.js
@@ -1,101 +1,47 @@
-const renderMenu = function() { 
 
-let banner = document.createElement("div")
-content.appendChild(banner)
+const renderMenu = function () { 
 
-let descriptionContent = document.createElement("div")
-content.appendChild(descriptionContent)
+    content.innerHTML = ""
 
-let hoursContent = document.createElement("div")
-content.appendChild(hoursContent)
+    let menuGrid = document.createElement("div")
+    content.appendChild(menuGrid)
 
-let locationContent = document.createElement("div")
-content.appendChild(locationContent)
+    for ( let i=0; i < 6; i++) {
 
-banner.style.backgroundColor = "brown"
-banner.style.color = "black"
-banner.style.outline = "solid 10px black"
+        let menuCell = document.createElement("div")
+        menuCell.id = `CellNumber${i}`
 
-banner.style.height = "8vh"
-banner.style.width = "60vw"
-banner.style.fontSize = "2rem"
-banner.style.padding = "2%"
-banner.style.borderRadius = "35%"
-banner.style.textAlign = "center"
-banner.style.fontWeight = "bold"
+        menuCell.style.backgroundColor = "white"
 
-banner.style.margin = "auto"
-banner.style.marginTop = "6%"
+        menuCell.style.width = "100%"
+        menuCell.style.height = "100%"
 
-banner.innerHTML = "Welcome to The Generic Restaurant!"
+        menuGrid.appendChild(menuCell)
+    }
 
+    const images = ['/src/images/genericBurger.jpg','/src/images/genericGarlicBread.jpg','/src/images/genericJollof.jpg','/src/images/genericMozzStick.jpg',
+    '/src/images/genericPizza.webp', '/src/images/genericSoup.jpg']
+    
+    for ( let i=0; i < 6; i++) {    
+        let currentImage = document.createElement("img")
+        currentImage.src = images[i]
+        
+        let currentCell = document.getElementById(`CellNumber${i}`)
+        currentCell.appendChild(currentImage)
+    }
 
-descriptionContent.style.backgroundColor = "brown"
-descriptionContent.style.color = "black"
-descriptionContent.style.outline = "solid 5px black"
+    menuGrid.style.display = "grid"
+    menuGrid.style.gap = "1rem"
+    menuGrid.style.gridTemplateColumns = "repeat(3,auto)"
+    menuGrid.style.gridTemplateRows = "repeat(2,auto)"
 
-descriptionContent.style.height = "40vh"
-descriptionContent.style.width = "20vw"
-descriptionContent.style.fontSize = "1.2rem"
-descriptionContent.style.padding = "3%"
-descriptionContent.style.borderRadius = "10%"
-descriptionContent.style.textAlign = "center"
+    menuGrid.style.backgroundColor = "beige"
 
-descriptionContent.style.margin = "auto"
-descriptionContent.style.marginTop = "6%"
-
-descriptionContent.innerHTML = `
-  <p>From our generic founding, the generic restaurant has always tried to be as generic as humanly possible. 
-  we inherited this ethos from our founder, John Generic, and his forward thinking attitude of success, 
-  virtue and hard work in all things generic has served us well. </p>
-  <h2>- Generic Co.</h2>
-`
-
-
-hoursContent.style.backgroundColor = "brown"
-hoursContent.style.color = "black"
-hoursContent.style.outline = "solid 10px white"
-
-hoursContent.style.height = "70vh"
-hoursContent.style.width = "26vw"
-hoursContent.style.padding = "2rem"
-hoursContent.style.paddingTop = "0.2rem"
-hoursContent.style.fontSize = "1.5rem"
-hoursContent.style.borderRadius = "35%"
-hoursContent.style.textAlign = "center"
-
-hoursContent.style.margin = "auto"
-hoursContent.style.marginTop = "6%"
-
-hoursContent.innerHTML = `<u><strong><h3> Hours</h3></u>
-<ul> Monday: 9am - 11pm </ul>
-<ul> Tuesday: 9am - 11pm </ul>
-<ul> Wednesday: 9am - 11pm </ul>
-<ul> Thursday: 9am - 11pm </ul>
-<ul> Friday: 9am - 11pm </ul>
-<ul> Saturday: 10am - 4pm </ul>
-<ul> Sunday: 10am - 4pm </ul> </strong>
-`
-
-
-locationContent.style.backgroundColor = "brown"
-locationContent.style.color = "black"
-locationContent.style.outline = "solid 5px black"
-
-locationContent.style.height = "auto"
-locationContent.style.width = "40vw"
-locationContent.style.padding = "1.5rem"
-locationContent.style.fontSize = "1.2rem"
-locationContent.style.borderRadius = "35%"
-
-locationContent.style.margin = "auto"
-locationContent.style.marginTop = "3%"
-locationContent.style.marginBottom = "20%"
-
-locationContent.innerHTML = ` <h2> Location </h2> 
-<p> 108-110 Sauchiehall Street, Glasgow, G2 3DE. </p> 
-`
-
+    menuGrid.style.height = "80vh"
+    menuGrid.style.width = "80vw"
+    menuGrid.style.marginLeft = "auto"
+    menuGrid.style.marginRight = "auto"
+    menuGrid.style.marginTop = "5%"
 }
 
 export {renderMenu}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import { renderPage } from "./mainModule"
+import { renderPage } from "./mainModule.js"
+import { renderHome } from "./Home.js"
 import { renderMenu } from "./Menu.js"
 
 renderPage()
-renderMenu() 
+renderHome()
+
+let homeTab = document.querySelector("#homeButton")
+let menuTab = document.querySelector("#menuButton")
+let aboutTab = document.querySelector("#aboutButton")
+let careersTab = document.querySelector("#careersButton")
+
+homeTab.addEventListener("click", () => { renderHome() })
+menuTab.addEventListener("click", () => { renderMenu() })
+aboutTab.addEventListener("click", () => { })
+careersTab.addEventListener("click", () => { })
+
